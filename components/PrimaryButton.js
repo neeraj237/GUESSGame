9de8a04d7983c0b5fee58cffa478b7e4e9ffbe08
neeraj237 <neeraj.237@gmail.com>
2024-12-11 19:24:59 +0530

    base of android game

---
+++ components/PrimaryButton.js
@@ -0,0 +1,26 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+function PrimaryButton({children}) {
+    function pressHandler() {
+        console.log('Presses!!')
+    }
+    return( 
+        <Pressable onPress={pressHandler}>
+        <View style={styles.container}>
+        <Text>{children}</Text>
+        </View>
+        </Pressable>
+    );
+    
+}
+
+export default PrimaryButton;  
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    }
+})

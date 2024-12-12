513530fe3c6cfced643153f1b24844d7b814d0f0
neeraj237 <neeraj.237@gmail.com>
2024-12-12 19:48:54 +0530

    Middle of Game Program

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,14 +1,15 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
-function PrimaryButton({children}) {
+function PrimaryButton({children, onPress}) {
     function pressHandler() {
         console.log('Presses!!')
     }
     return( 
-        <Pressable onPress={pressHandler}>
-        <View style={styles.container}>
-        <Text>{children}</Text>
-        </View>
+        <View style={styles.buttonOuterContainer}>
+        <Pressable style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} 
+        onPress={onPress} android_ripple={{color: '#644202'}}>
+        <Text style = {styles.buttonText}>{children}</Text>
         </Pressable>
+        </View>
     );
     
 }
@@ -16,11 +17,22 @@ function PrimaryButton({children}) {
 export default PrimaryButton;  
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#72063c',
+    buttonOuterContainer: {
         borderRadius: 28,
+        margin:4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
         paddingVertical: 8,
         paddingHorizontal: 16,
         elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
     }
 })
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,44 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from '../components/PrimaryButton.js';
+import { useState } from "react";
+function StartGameScreen({onPickNumber}) {
+    const [enteredNumber, setEnteredNumber] = useState('');
+    function numberInputHandler(enteredText) {
+     setEnteredNumber(enteredText);
+    }
 
-function StartGameScreen() {
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
+    function confirmInputHandler() {
+       const chosenNumber = parseInt(enteredNumber);
+       if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+        Alert.alert('Invalid Number', 'Number has to be a number between 1 and 99',
+            [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+        );
+        return;
+       }
+       onPickNumber(chosenNumber);
+       //console.log('Valid Number !!')
+    }
+
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" 
+            autoCapitalize="none" autoCorrect={false}
+            onChangeText={numberInputHandler}
+            value={enteredNumber}
+            />
+        
+        <View style={styles.buttonsContainer}>
+            <View style= {styles.buttonContainer}>
+            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+            </View>
+            <View style= {styles.buttonContainer}>
+            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+            </View>
+        </View>
         </View>
     );
 
@@ -16,10 +48,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: 'center',
+        alignItems:'center',
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: '#3b021f',
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
@@ -37,5 +71,11 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
     }
 });

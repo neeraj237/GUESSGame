787644b75889706eaf9afa27df40207ad87b2d93
neeraj237 <neeraj.237@gmail.com>
2024-12-13 18:52:59 +0530

    Second commit for GuessNumber game

--- App.js
+++ App.js
@@ -1,29 +1,66 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet,ImageBackground } from 'react-native';
+import { StyleSheet,ImageBackground, SafeAreaView } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+import Colors from './constants/colors';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+  
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>;
+  }
 
   function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber)
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+
+  function startNewGameHandler() {
+   setUserNumber(null);
+   setGuessRounds(0);
+
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
 
   if (userNumber) {
-    screen = <GameScreen  />
+    screen = (
+    <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>
+    );
   }
 
+  if(gameIsOver && userNumber) {
+    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
+  }
+
+  
+
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient colors={[Colors.primary700,Colors.accent500]} style={styles.rootScreen}>
       <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover'
       style={styles.rootScreen}
       imageStyle={styles.backgroundImage}
       >
+        <SafeAreaView style={styles.rootScreen}>
   {screen}
+  </SafeAreaView>
   </ImageBackground>
 
   </LinearGradient>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,10 @@
 import { TextInput, View, StyleSheet, Alert } from "react-native";
-import PrimaryButton from '../components/PrimaryButton.js';
+import PrimaryButton from "../components/ui/PrimaryButton.js";
 import { useState } from "react";
+import Colors from "../constants/colors.js";
+import Card from "../components/ui/Card.js";
+import Title from "../components/ui/Title.js";
+import InstructionText from "../components/ui/InstructionText.js";
 function StartGameScreen({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState('');
     function numberInputHandler(enteredText) {
@@ -24,7 +28,10 @@ function StartGameScreen({onPickNumber}) {
     }
 
     return (
-        <View style={styles.inputContainer}>
+        <View style={styles.rootContainer}>
+            <Title>Guess My Number</Title>
+            <Card>
+                  <InstructionText>Enter a Number</InstructionText> 
             <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" 
             autoCapitalize="none" autoCorrect={false}
             onChangeText={numberInputHandler}
@@ -39,7 +46,9 @@ function StartGameScreen({onPickNumber}) {
             <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
             </View>
         </View>
+        </Card>
         </View>
+       
     );
 
 }
@@ -47,27 +56,19 @@ export default StartGameScreen;
 
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        justifyContent: 'center',
-        alignItems:'center',
+    rootContainer: {
+        flex: 1,
         marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#3b021f',
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25
+        alignItems: 'center'
     },
+
     numberInput: {
         height: 60,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
